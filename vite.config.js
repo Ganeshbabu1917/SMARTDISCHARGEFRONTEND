042593d9ccc805515d72f8b0df539f2ea614e205
Ganeshbabu1917 <sundaraganeshbabu1917@gmail.com>
2026-02-28 23:16:09 +0530

    Added base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-        open: true
-    }
+  base: '/SMARTDISCHARGEFRONTEND/',   // 👈 Add this line
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  }
 })
